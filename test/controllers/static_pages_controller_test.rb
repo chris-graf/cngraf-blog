62ed48a2b89c3e7ddd6d285e1b7ef98ccabc2dda
Chris Graf <cgraf@uchicago.edu>
2015-09-01 22:44:50 -0500

    add title tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "The Blog of Chris Graf | Home"
   end
 
   test "should get projects" do
     get :projects
     assert_response :success
+    assert_select "title", "The Blog of Chris Graf | Projects"
   end
 
 end
